b4f0f44271037103c37de21de0ee20f724ef76bb
Alex Nagel <awnagel700@gmail.com>
2023-12-05 11:56:08 +0100

    Added demo reading excel data.

---
+++ main.R
@@ -0,0 +1,16 @@
+# install.packages("readxl")
+library("readxl")
+
+data = read_excel("data/Baltimore_1910_ScarletFever.xlsx");
+print(data)
+
+rstudio_clear_plots = function()
+{
+	# If RStudio has created any plots, clear them.
+    if (exists("dev")) {
+        x = dev.list()["RStudioGD"]
+        if (!is.null(x)) { dev.off(x) }    
+    }
+}
+
+rstudio_clear_plots()
