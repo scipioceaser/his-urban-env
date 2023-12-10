327f999a2f6e3a6a6b279fad6e059ab6096c648c
Alex Nagel <awnagel700@gmail.com>
2023-12-10 18:24:38 +0100

    Update gis_experimental.R

--- gis_experimental.R
+++ gis_experimental.R
@@ -1,7 +1,8 @@
 library(ggmap)
 library(ggplot2)
 library(sf)
-library("data.table")
+library(data.table)
+library(readxl)
 
 # TODO: Put this into a common utility script.
 rstudio_clear_plots = function()
@@ -25,15 +26,41 @@ working_dir_setlocal()
 water_shape = read_sf(dsn = "data/GIS/Baltimore",
                       layer = "CPE_Baltimore_Sanitation_Infrastructure")
 
-ward_shape = read_sf(dsn = "data/GIS/Baltimore", layer = "baltimore_wards_1919_1930")
+# TODO: Check and make sure that the ward *numbers* remain consistent.
+ward_shape = read_sf(dsn = "data/GIS/Baltimore", layer = "baltimore_wards_1902_1918")
 
 water_shape_frame = as.data.table(water_shape)
-water_shape_frame = water_shape_frame[WaterDate > 0]
+water_shape_frame = water_shape_frame[WaterDate > 0 & WaterDate < 1918]
+
+raw_ward_data = read_excel("data/Baltimore_1910_ScarletFever.xlsx")
+
+# So far so good, GIS data seems to be a bit better organized than Excel data.
+
+get_fever_row_sum = function(offset)
+{
+	# TODO: There has to be some sort of way that we can optimize this.
+	ward = lapply(raw_ward_data[6 + offset, 50:53], gsub, pattern=".....", replacement=0)
+	ward = sum(as.numeric(ward))
+	return(ward)
+}
+
+ws = as.data.table(ward_shape)
+ws = ws[order(Ward_Num)]
+
+ws$fever = seq(from = 1, to = 24)
+for (i in 0:23) {
+	ws$fever[i + 1] = get_fever_row_sum(i)
+}
 
 ggplot() +
-    geom_sf(data = ward_shape, alpha = 0.8, aes(fill = Ward_Num)) +
+    geom_sf(data = ward_shape, alpha = 0.8) +
+    scale_fill_viridis_c() +
+    geom_sf(data = st_as_sf(ws), aes(fill = fever)) +
+    geom_sf(data = st_as_sf(water_shape_frame), aes(color = WaterDate))
+
     # geom_sf_label(data = ward_shape, aes(label = Ward_Num)) +
-    geom_sf(data = water_shape) +
-    geom_sf(data = st_as_sf(water_shape_frame[WaterDate > 1890 & WaterDate <= 1900]), color = "green") +
-    geom_sf(data = st_as_sf(water_shape_frame[WaterDate > 1900]), color = "yellow") +
-    geom_sf(data = st_as_sf(water_shape_frame[WaterDate <= 1890]), color = "red")
+    # geom_sf(data = water_shape) +
+
+    # geom_sf(data = st_as_sf(water_shape_frame[WaterDate > 1890 & WaterDate <= 1900]), color = "green") +
+    # geom_sf(data = st_as_sf(water_shape_frame[WaterDate > 1900]), color = "yellow") +
+    # geom_sf(data = st_as_sf(water_shape_frame[WaterDate <= 1890]), color = "red")
