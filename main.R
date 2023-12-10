0fc593e5b567a49f939bbf2b14d6a346873af66b
Alex Nagel <awnagel700@gmail.com>
2023-12-10 22:31:33 +0100

    Tried to fix issue with water borne.

--- main.R
+++ main.R
@@ -1,8 +1,11 @@
 # install.packages("readxl")
 library("readxl")
 
-data = read_excel("data/Baltimore_1910_ScarletFever.xlsx");
-print(data)
+working_dir_setlocal = function()
+{
+	# From: https://stackoverflow.com/questions/13672720/
+	setwd(getSrcDirectory(function(){})[1])
+}
 
 rstudio_clear_plots = function()
 {
@@ -14,27 +17,29 @@ rstudio_clear_plots = function()
 }
 
 rstudio_clear_plots()
+working_dir_setlocal()
 
-install.packages(c("readxl", "data.table", "dplyr"))
+data = read_excel("data/Baltimore_1910_ScarletFever.xlsx");
+print(data)
+
+# install.packages(c("readxl", "data.table", "dplyr"))
 library("readxl")
 
 library("data.table")
 
-
-
-data1890jan = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ - Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6120,_1890-Philadelphia,_Interments_in_the_City_of_Philadelphia_January_1890,_p358-79.xls")
-data1890feb = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6121,_1890-Philadelphia,_Interments_in_the_City_of_Philadelphia_February_1890,_1890,_p382-01.xls")
-data1890mar = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6123,_1890-Philadelphia,_Interments_in_the_the_City_of_Philadelphia_March_1890,_p404-21.xls")
-data1890apr = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6124,_1890-Philadelphia,_Interments_in_the_City_of_Philadelphia_April_1890,_1890,_p424-42.xls")
-data1890may = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6125,_1890-Philadelphia,_Interments_in_the_City_of_Philadelphia_May_1890,_1890,_p446-65.xls")
-data1890jun = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6126,_1890-Philadelphia,_Interments_June_1890,_1890,_p468-87.xls")
-data1890jul = read_excel("/Users/robertjastrzebski/Desktop/Utrecht RMA/Methods - Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6141,_1890-Philadelphia,_Interments_July_1890,_pg_490-09.xls")
-data1890aug = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6142,_1890-Philadelphia,_Interments_August_1890,_p512-29.xls")
-data1890sep = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6155,_1890-Philadelphia,_Interments_September_1890,_1890,_p532-47.xls")
-data1890oct = read_excel("/Users/robertjastrzebski/Desktop/Utrecht RMA/Methods - Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6156,_1890-Philadelphia,_Interment_in_the_City_of_Philadelphia_October_1890,_1890,_p550-67.xls")
+data1890jan = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6120,_1890-Philadelphia,_Interments_in_the_City_of_Philadelphia_January_1890,_p358-79.xls")
+data1890feb = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6121,_1890-Philadelphia,_Interments_in_the_City_of_Philadelphia_February_1890,_1890,_p382-01.xls")
+data1890mar = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6123,_1890-Philadelphia,_Interments_in_the_the_City_of_Philadelphia_March_1890,_p404-21.xls")
+data1890apr = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6124,_1890-Philadelphia,_Interments_in_the_City_of_Philadelphia_April_1890,_1890,_p424-42.xls")
+data1890may = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6125,_1890-Philadelphia,_Interments_in_the_City_of_Philadelphia_May_1890,_1890,_p446-65.xls")
+data1890jun = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6126,_1890-Philadelphia,_Interments_June_1890,_1890,_p468-87.xls")
+data1890jul = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6141,_1890-Philadelphia,_Interments_July_1890,_pg_490-09.xls")
+data1890aug = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6142,_1890-Philadelphia,_Interments_August_1890,_p512-29.xls")
+data1890sep = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6155,_1890-Philadelphia,_Interments_September_1890,_1890,_p532-47.xls")
+data1890oct = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6156,_1890-Philadelphia,_Interment_in_the_City_of_Philadelphia_October_1890,_1890,_p550-67.xls")
 
 #We are missing data for November - Likely does not make a huge difference as waterborne illnesses generally peak during periods of warm weather#
-data1890dec = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6171,_1890-Philadelphia,_Interments_December_1890,_1890,_p590-07.xls")
+data1890dec = read_excel("data/philadelphia/Philadelphia_1890/1890_WL-SID452,_TID6171,_1890-Philadelphia,_Interments_December_1890,_1890,_p590-07.xls")
 
 ##1890
 
@@ -55,12 +60,12 @@ water_borne1890dec
 #fy is full year, our data from 1890 is monthly, denotes the difference in data recording#
 
 ##1900
-data1900fy = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1900/1900_WL-303_3112_1900-Deaths_by_Cause_1900,_p156-95.xls")
+data1900fy = read_excel("data/philadelphia/Philadelphia_1900/1900_WL-303_3112_1900-Deaths_by_Cause_1900,_p156-95.xls")
 
 water_borne1900fy <- rbind(data1900fy[84, ], data1900fy[85, ], data1900fy[122, ], data1900fy[138, ])
 
 ##1910
-data1910fy = read_excel("/Users/robertjastrzebski/Desktop/Utrecht\ RMA/Methods\ -\ Quantitative/Project/philadelphia/Philadelphia_1910/1910_WL-SID314,_TID3344,_1910-Philadelphia,_Causes_of_Death_Distributed_By_Ward_1910,_pgs_unknown.xlsx")
+data1910fy = read_excel("data/philadelphia/Philadelphia_1910/1910_WL-SID314,_TID3344,_1910-Philadelphia,_Causes_of_Death_Distributed_By_Ward_1910,_pgs_unknown.xlsx")
 
 water_borne1910fy <- rbind(data1910fy[8, ], data1910fy[18, ], data1910fy[19, ])
 
@@ -69,44 +74,63 @@ object_list <- list(water_borne1890jan, water_borne1890feb, water_borne1890mar,
                     water_borne1890jul, water_borne1890aug, water_borne1890sep, 
                     water_borne1890oct, water_borne1890dec)
 
-object_list <- lapply(object_list, function(x) x[is.numeric(x)])
-                    
-sum_waterborne1890 <- Reduce('+', object_list)
+# NOTE(Alex): Same as before, we merge together all the month data frames.
+water_borne = as.data.table(rbind(Reduce(function(x, y) merge(x, y, all=TRUE), object_list)))
 
-#Could not sum because of presence of non-numeric values, so used 
-#custom function
+# NOTE(Alex): Then we replace all the garbage values with 0.
+#       Whoever decided to insert two different strings to represent NA, I am going to strangle.
+water_borne[water_borne == "….."] <- 0
+water_borne[water_borne == "....."] <- 0
+water_borne[water_borne == "…../..."] <- 0
+water_borne[is.na(water_borne)] <- 0
+water_borne[water_borne == "2....."] = 2
+
+# Transpose the frame (swap rows and columns)
+# Alright, so the 27:59 things is because the ward level data is on columns 29 through 61,
+#       and in the test frame those numbers have had 2 subtracted for some reason.
+#       Fairly, but not absolutely, certain that this is good.
+water_borne_trans = t(water_borne[, 27:59])
+
+# Assign column names to diseases
+colnames(water_borne_trans) = water_borne$PHILADELPHIA
+
+# Remove top row (which is just disease names)
+water_borne_trans = water_borne_trans[c(-1), ]
+
+# Convert all the columns to numeric, and sum all the duplicate columns together
+water_borne_numeric = apply(water_borne_trans, 2, as.numeric)
+water_borne_numeric = sapply(unique(colnames(water_borne_numeric)), function(x) rowSums(water_borne_numeric[, grepl(x, colnames(water_borne_numeric))]))
 
 # Custom function to convert columns to numeric, handling non-numeric characters
-convert_to_numeric <- function(df) {
-  for (i in 1:ncol(df)) {
-    if (!is.numeric(df[[i]])) {
-      df[[i]] <- as.numeric(as.character(df[[i]]))
+if (F) {
+    convert_to_numeric <- function(df) {
+      for (i in 1:ncol(df)) {
+        if (!is.numeric(df[[i]])) {
+          df[[i]] <- as.numeric(as.character(df[[i]]))
+        }
+      }
+      return(df)
     }
-  }
-  return(df)
+    
+    
+    water_borne1890jan_numeric <- convert_to_numeric(water_borne1890jan)
+    water_borne1890feb_numeric <- convert_to_numeric(water_borne1890feb)
+    water_borne1890mar_numeric <- convert_to_numeric(water_borne1890mar)
+    water_borne1890apr_numeric <- convert_to_numeric(water_borne1890apr)
+    water_borne1890may_numeric <- convert_to_numeric(water_borne1890may)
+    water_borne1890jun_numeric <- convert_to_numeric(water_borne1890jun)
+    water_borne1890jul_numeric <- convert_to_numeric(water_borne1890jul)
+    water_borne1890aug_numeric <- convert_to_numeric(water_borne1890aug)
+    water_borne1890sep_numeric <- convert_to_numeric(water_borne1890sep)
+    water_borne1890oct_numeric <- convert_to_numeric(water_borne1890oct)
+    water_borne1890dec_numeric <- convert_to_numeric(water_borne1890dec)
+    
+    object_list <- list(water_borne1890jan_numeric, water_borne1890feb_numeric, water_borne1890mar_numeric,
+                        water_borne1890apr_numeric, water_borne1890may_numeric, water_borne1890jun_numeric,
+                        water_borne1890jul_numeric, water_borne1890aug_numeric, water_borne1890sep_numeric, 
+                        water_borne1890oct_numeric, water_borne1890dec_numeric)
+    
+    sum_waterborne1890 <- Reduce('+', object_list)
+    
+    sum_waterborne1890
 }
-
-
-water_borne1890jan_numeric <- convert_to_numeric(water_borne1890jan)
-water_borne1890feb_numeric <- convert_to_numeric(water_borne1890feb)
-water_borne1890mar_numeric <- convert_to_numeric(water_borne1890mar)
-water_borne1890apr_numeric <- convert_to_numeric(water_borne1890apr)
-water_borne1890may_numeric <- convert_to_numeric(water_borne1890may)
-water_borne1890jun_numeric <- convert_to_numeric(water_borne1890jun)
-water_borne1890jul_numeric <- convert_to_numeric(water_borne1890jul)
-water_borne1890aug_numeric <- convert_to_numeric(water_borne1890aug)
-water_borne1890sep_numeric <- convert_to_numeric(water_borne1890sep)
-water_borne1890oct_numeric <- convert_to_numeric(water_borne1890oct)
-water_borne1890dec_numeric <- convert_to_numeric(water_borne1890dec)
-
-
-
-object_list <- list(water_borne1890jan_numeric, water_borne1890feb_numeric, water_borne1890mar_numeric,
-                    water_borne1890apr_numeric, water_borne1890may_numeric, water_borne1890jun_numeric,
-                    water_borne1890jul_numeric, water_borne1890aug_numeric, water_borne1890sep_numeric, 
-                    water_borne1890oct_numeric, water_borne1890dec_numeric)
-
-sum_waterborne1890 <- Reduce('+', object_list)
-
-sum_waterborne1890
-
